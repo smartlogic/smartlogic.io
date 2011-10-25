ad4069cd3e1316ac0213b57838baeb34431ceb06
Yair Flicker <yair@smartlogicsolutions.com>
2011-10-25 12:23:42 -0400

    First go at some HTML/CSS for the new site.

---
+++ javascript.js
@@ -0,0 +1,35 @@
+/** Hides articles in favor of another **/
+function showArticle(article) {
+	$('article').hide();
+	$('article' + article).show();
+	document.title = $('article' + article + ' h2').html() + ' :: SmartLogic';
+}
+
+/** Loads selected article and portfolio item, if any **/
+function loadStateFromHash() {
+	if (window.location.hash) {
+		var article = (window.location.hash.indexOf('/') > 0) ?
+			window.location.hash.substring(0, window.location.hash.indexOf('/')) :
+			window.location.hash;
+		if ($('article' + article)) {
+			showArticle(article);
+		}
+	} else {
+		showArticle('#about');
+	}
+}
+
+$(document).ready(function() {
+	
+	// load initial state
+	loadStateFromHash();
+	
+	// detect when the hash changes and react accordingly --> catch fwd/back buttons
+	$(window).bind('hashchange', loadStateFromHash);
+	
+	// when you click a nav link show the associated article
+	$('body > nav > a').click(function() {
+		showArticle($(this).attr('href'));
+	});
+	
+});
