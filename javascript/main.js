1d5d90f5f6a5af50edd239c1955f7f89be1f1abd
Ryan DeStefano <ryan.j.destefano@gmail.com>
2014-08-04 11:38:36 -0700

    Added opactiy changes to sliding navigation menues

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $('.sidebar a').click(function() {
     $('.primary-navigation').toggleClass('active', 1000, 'easeOutCubic');
+    $('.primary-navigation > *').delay(250).toggleClass('active', 750, 'easeOutCubic');
   });
 
 });
