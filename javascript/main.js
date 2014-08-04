58ece439f39c6d4eb930b2d518225475668c2794
Ryan DeStefano <ryan.j.destefano@gmail.com>
2014-08-04 16:22:57 -0700

    Added Circular font

--- javascript/main.js
+++ javascript/main.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
 
   $('.slider-control').click(function() {
     if($('.current-slide').next('.slide').length) {
-      $('.current-slide').addClass('previous-slide', sliderSpeed).removeClass('current-slide').next('.slide').addClass('current-slide', sliderSpeed);
+      $('.current-slide').addClass('previous-slide', sliderSpeed, 'easeOutCubic').removeClass('current-slide').next('.slide').addClass('current-slide', sliderSpeed, 'easeOutCubic');
     }
     else {
-      $('.current-slide').addClass('previous-slide', sliderSpeed).removeClass('current-slide');
-      $('.slide:first-child').addClass('current-slide', sliderSpeed);
+      $('.current-slide').addClass('previous-slide', sliderSpeed, 'easeOutCubic').removeClass('current-slide');
+      $('.slide:first-child').addClass('current-slide', sliderSpeed, 'easeOutCubic');
     }
 
     $('.slide').removeClass('previous-slide');
