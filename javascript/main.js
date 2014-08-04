31a962bce9a4bb83f5eee02ad7b04d6e3272e9ad
Ryan DeStefano <ryan.j.destefano@gmail.com>
2014-08-04 15:08:58 -0700

    Set up Home page slider
    * Slides scroll vertically and loop when the last slide has been reached.

--- javascript/main.js
+++ javascript/main.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
     $('.primary-navigation > *').delay(250).toggleClass('active', 750, 'easeOutCubic');
   });
 
+  // HOME PAGE SLIDER
+
+  var sliderSpeed = 1000;
+
+  $('.slider-control').click(function() {
+    if($('.current-slide').next('.slide').length) {
+      $('.current-slide').addClass('previous-slide', sliderSpeed).removeClass('current-slide').next('.slide').addClass('current-slide', sliderSpeed);
+    }
+    else {
+      $('.current-slide').addClass('previous-slide', sliderSpeed).removeClass('current-slide');
+      $('.slide:first-child').addClass('current-slide', sliderSpeed);
+    }
+
+    $('.slide').removeClass('previous-slide');
+  });
+
 });
