5647ed36cf0ec777127cc8da7dbed74060184545
rozerosie <rozerosie@users.noreply.github.com>
2020-10-26 15:05:09 +0000

    deploy: f9f34d37bb565be03c61a50b81582ff58883f891

--- javascript/map.js
+++ javascript/map.js
@@ -1,4 +1,8 @@
 //  Google Map
+
+//  note: not sure why but there is a duplicate script in main.js; stops working when I delete one or the other
+//  so leaving as-is. Just remember to update both if you are changing the address location
+
 var map;
 function initMap() {
   var myLatLng = {lat: 39.288550, lng: -76.606950};
