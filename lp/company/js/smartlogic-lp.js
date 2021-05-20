429ea91ef40c7c2ec6a789260c6810d6419cb0a9
Rose Burt <rose@smartlogic.io>
2021-05-20 12:41:09 -0400

    add ads landing page

---
+++ lp/company/js/smartlogic-lp.js
@@ -0,0 +1,7 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
