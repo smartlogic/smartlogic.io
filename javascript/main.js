852c19e5f00198c8452cbb75c6155116df9ea613
Emma Whamond <emma@smartlogic.io>
2024-08-30 07:08:58 -0500

    Add "at a glance" section and get started modal to landing page

--- javascript/main.js
+++ javascript/main.js
@@ -29,3 +29,72 @@ hamburger.addEventListener('click', function(){
   }
   header.classList.toggle('header-hamburger-open');
 })
+
+// "Get Started" Landing page modal 
+document.addEventListener('DOMContentLoaded', function () {
+  var modal = document.getElementById('modal');
+  var btn = document.getElementById('openModal');
+  var span = document.getElementsByClassName('close')[0];
+
+  btn.onclick = function () {
+    modal.style.display = 'block';
+  }
+
+  span.onclick = function () {
+    modal.style.display = 'none';
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+});
+
+
+// Landing page pillar cards
+document.addEventListener('DOMContentLoaded', function () {
+  const previousCard = document.getElementById("back-arrow");
+  const nextCard = document.getElementById("next-arrow");
+
+  nextCard.addEventListener("click", showNextCard);
+  previousCard.addEventListener("click", showPreviousCard);
+
+  function showNextCard() {
+    const currentCardId = document.getElementsByClassName("current-card")[0].id;
+    const currentCardNb = Number(currentCardId[currentCardId.length - 1]);
+
+    const displayedCardId = `card-${currentCardNb}`;
+    const displayedCard = document.getElementById(displayedCardId);
+
+    let nextCardNb = currentCardNb + 1;
+    if (nextCardNb === 5) nextCardNb = 1;
+
+    const cardToBeDisplayedId = `card-${nextCardNb}`;
+    const cardToBeDisplayed = document.getElementById(cardToBeDisplayedId);
+
+    displayedCard.classList.remove("current-card");
+    displayedCard.classList.add("hidden-card");
+    cardToBeDisplayed.classList.remove("hidden-card");
+    cardToBeDisplayed.classList.add("current-card");
+  }
+
+  function showPreviousCard() {
+    const currentCardId = document.getElementsByClassName("current-card")[0].id;
+    const currentCardNb = Number(currentCardId[currentCardId.length - 1]);
+
+    const displayedCardId = `card-${currentCardNb}`;
+    const displayedCard = document.getElementById(displayedCardId);
+
+    let previousCardNb = currentCardNb - 1;
+    if (previousCardNb === 0) previousCardNb = 4;
+
+    const cardToBeDisplayedId = `card-${previousCardNb}`;
+    const cardToBeDisplayed = document.getElementById(cardToBeDisplayedId);
+
+    displayedCard.classList.remove("current-card");
+    displayedCard.classList.add("hidden-card");
+    cardToBeDisplayed.classList.remove("hidden-card");
+    cardToBeDisplayed.classList.add("current-card");
+  }
+});
